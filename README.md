cb1549d8f31ac2f2e813149284c1bccc9442f94c
Aaron99P <picoaaron@gmail.com>
2022-07-25 12:13:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TFM-Federated-Learning-and-ESN
---
+++ v1/dataset.py
@@ -0,0 +1,144 @@
+import numpy as np
+import math
+n_steps = 15
+n_features = 1
+
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return X, y
+
+
+def sequence(df, n):
+	seq_aux = df.iloc[:, n].tolist()
+
+	seq = []
+	for elem in seq_aux:
+		if elem != 0.0 and not np.isnan(elem):
+			seq.append(elem)
+	
+	x, y = split_sequence(seq, n_steps)
+	
+	x = np.array(x)
+	y = np.array(y)
+
+	x = x.reshape((x.shape[0], x.shape[1], n_features))
+
+	return x, y
+
+
+def sequence2(df, n):
+	seq_aux = df.iloc[:, n].tolist()
+
+	seq = []
+	for elem in seq_aux:
+		if np.isnan(elem):
+				seq.append(0.0)
+		else:
+			seq.append(elem)
+	
+	x, y = split_sequence(seq, n_steps)
+	
+	x = np.array(x)
+	y = np.array(y)
+
+	x = x.reshape((x.shape[0], x.shape[1], n_features))
+
+	return x, y
+
+
+def sequence_many(df, n1, n2):
+	x = []
+	y = []
+	
+	for i in range(n1, n2):
+		seq_aux = df.iloc[:, i].tolist()
+
+		seq = []
+		for elem in seq_aux:
+			if elem != 0.0 and not np.isnan(elem):
+				seq.append(elem)
+
+		seq_x, seq_y = split_sequence(seq, n_steps)
+
+		x += seq_x
+		y += seq_y
+	
+	x = np.array(x)
+	y = np.array(y)
+
+	x = x.reshape((x.shape[0], x.shape[1], n_features))
+
+	return x[:5000], y[:5000]
+
+
+
+def distance(c1, c2):
+  x = ( c1[0] - c2[0] ) ** 2
+  y = ( c1[1] - c2[1] ) ** 2
+  return math.sqrt( x + y )
+
+
+
+def adjacency(data, n):
+	ad_matrix = []
+	neighbors_list = {}
+
+	for wind in data:
+		neighbors = []
+		nodes = [ (x, data[x]['coordinates']) for x in data] 
+
+		for i in range(n+1):
+
+			closest = nodes[0]
+			dist_closest = distance(data[wind]['coordinates'], nodes[0][1])
+
+			for nd in nodes:
+				dist = distance( data[wind]['coordinates'], nd[1])
+				if dist < dist_closest:
+					dist_closest = dist
+					closest = nd
+
+			if wind != closest[0]:
+				neighbors.append(closest[0])
+				
+			nodes.remove(closest)
+
+		neighbors_list.update({ wind: neighbors})
+		ad_matrix.append( [1 if x in neighbors else 0 for x in data] )
+
+	return np.array(ad_matrix), neighbors_list
+
+
+
+
+def wind_data(df, name):
+
+	seq_aux = df[name]
+
+	seq = []
+	for elem in seq_aux:
+		if np.isnan(elem):
+				seq.append(0.0)
+		else:
+			seq.append(elem)
+	
+	x, y = split_sequence(seq, n_steps)
+	
+	x = np.array(x)
+	y = np.array(y)
+
+	x = x.reshape((x.shape[0], x.shape[1], n_features))
+
+	return x, y
+
